e1d3367ffd24b90a588dd2bc69198818140c76be
Rodrigo-Fernandez11 <101433818+Rodrigo-Fernandez11@users.noreply.github.com>
2024-04-01 20:20:53 -0300

    navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,9 +12,9 @@ export const NavBar = () => {
   };
 
   return (
-    <nav className="">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+    <nav>
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+        <a href="#hero" className="flex items-center space-x-3 ">
           <Image
             src={Logo}
             height={50}
